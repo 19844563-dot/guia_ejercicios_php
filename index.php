56a3366c67e9158d88f6e5c864ba6f5d57df22d7
19844563-dot <19844563@clases.edu.sv>
2026-05-20 09:08:49 -0600

    index
    En este archivo estan los 25 ejercicios de html y php

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <body>
+
+        <title>Menu</title>
+<h2>Menu de ejercicios</h2>
+
+<p><a href="nombre2.html">ejercicio1 - siguiente</a>
+<p><a href="suma2.html">ejercicio2 - siguiente</a></p>
+<p><a href="resta2.html">ejercicio3 - siguiente</a></p>
+<p><a href="multiplicacion2.html">ejercicio4 - siguiente</a></p>
+<p><a href="divison2.html">ejercicio5 - siguiente</a></p>
+<p><a href="nombreedad.html">ejercicio6 - siguiente</a></p>
+<p><a href="mayor.html">ejercicio7 - siguiente</a></p>
+<p><a href="menor.html">ejercicio8 - siguiente</a></p>
+<p><a href="parimpar.html">ejercicio9 - siguiente</a></p>
+<p><a href="promedio.html">ejercicio10 - siguiente</a></p>
+<p><a href="notass.html">ejercicio11 - siguiente</a></p>
+<p><a href="base.html">ejercicio12 - siguiente</a></p>
+<p><a href="Perimetro.html">ejercicio13 - siguiente</a></p>
+<p><a href="Mcm.html">ejercicio14 - siguiente</a></p>
+<p><a href="celcius.html">ejercicio15 - siguiente</a></p>
+<p><a href="chamba.html">ejercicio16 - siguiente</a></p>
+<p><a href="descuento10.html">ejercicio17 - siguiente</a></p>
+<p><a href="total.html">ejercicio18 - siguiente</a></p>
+<p><a href="tablasdemultiplicar.html">ejercicio19 - siguiente</a></p>
+<p><a href="sumadeunnumero.html">ejercicio20 - siguiente</a></p>
+<p><a href="cantidaddeletras.html">ejercicio21 - siguiente</a></p>
+<p><a href="dollareuro.html">ejercicio22 - siguiente</a></p>
+<p><a href="iva.html">ejercicio23 - siguiente</a></p>
+<p><a href="dentrode10.html">ejercicio24 - siguiente</a></p>
+<p><a href="datos.html">ejercicio25 - siguiente</a></p>
+<p><a href="ladosdeuncuadrado.html">ejercicio26 - siguiente</a></p>
+
+</body>
+</html>
+
+
